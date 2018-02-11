f5aa688bdf669c06ca7fd2134a3cb9e55d1ae7f6
rmann3 <rmann3@mail.csuchico.edu>
2018-02-11 03:36:28 -0800

    forgot to add stuff

--- main_site/funny_or_not/views.py
+++ main_site/funny_or_not/views.py
@@ -4,5 +4,15 @@ from django.template import loader
 from django.urls import reverse
 from django.views import generic
 #from .models import Question, Choice
+from .models import Video, Comment
 
+#def index(request):
+#    output = 'test format'
+#    return HttpResponse(output)
 
+class IndexView(generic.ListView):
+    template_name = 'funny_or_not/index.html'
+    context_object_name = 'latest_video_list'
+
+    def get_queryset(self):
+        return Video.objects.order_by('-pub_date')[:5]
